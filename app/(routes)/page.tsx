ad8b24dcc0f657e098bbf6032d5bd228d4a3161f
Jo <jose_f_dirazar@hotmail.com>
2024-01-03 14:00:41 -0300

    ws

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -3,6 +3,8 @@ import getProducts from "@/actions/get-products";
 import ProductList from "@/components/product-list";
 import Billboard from "@/components/ui/billboard";
 import Container from "@/components/ui/container";
+import Image from "next/image";
+import Link from "next/link";
 
 export const revalidate = 0;
 
@@ -12,7 +14,10 @@ const HomePage = async () => {
   return (
     <Container>
       <div className="space-y-10 pb-10">
-        <Billboard data={billboard} />
+        {/* <Billboard data={billboard} /> */}
+        <div className="flex flex-col bg-neutral-900 rounded-xl mx-8 p-4 sm:p-6 lg:p-8 items-center m-3 font-bold justify-center ">
+          <Image className="rounded-lg overflow-hidden" src='/multicuotas-logo.jfif' width={200} height={100} alt="Multicuotas logo" />
+        </div>
         <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
           <ProductList title="Mas vendidos" items={products} />
         </div>
--- components/ui/theme-switch.tsx
+++ components/ui/theme-switch.tsx
@@ -3,20 +3,36 @@
 import { useTheme } from "@/providers/theme-context";
 import React from "react";
 import { BsMoon, BsSun } from "react-icons/bs";
+import Link from "next/link";
+import Image from "next/image";
 
 export default function ThemeSwitch() {
   const { theme, toggleTheme } = useTheme();
 
   return (
-    <button
-      className="fixed bottom-5 right-5 flex h-[3rem] w-[3rem] items-center justify-center rounded-full border-[2px] border-black border-opacity-40 bg-black text-white shadow-2xl backdrop-blur-[0.5rem] transition-all hover:scale-[1.15] active:scale-105 dark:bg-white"
-      onClick={toggleTheme}
-    >
-      {theme === "light" ? (
-        <BsMoon className="rounded-full bg-black text-xl font-bold text-white" />
-      ) : (
-        <BsSun className="text-xl dark:bg-white dark:text-black" />
-      )}
-    </button>
+    <>
+      <Link
+        href="https://wa.me/+5492494280688"
+        target="_blank"
+        className="fixed bottom-20 right-[1.2rem] flex h-[3rem] w-[3rem] items-center justify-center rounded-full border-[2px] border-[#35B721] border-opacity-90 bg-green-800"
+      >
+        <Image
+          src="/f3535dc3f95e71506f7c80755610176c.png"
+          width={30}
+          height={30}
+          alt="Whatsapp logo"
+        />
+      </Link>
+      <button
+        className="fixed bottom-5 right-5 flex h-[3rem] w-[3rem] items-center justify-center rounded-full border-[2px] border-black border-opacity-40 bg-black text-white shadow-2xl backdrop-blur-[0.5rem] transition-all hover:scale-[1.15] active:scale-105 dark:bg-white"
+        onClick={toggleTheme}
+      >
+        {theme === "light" ? (
+          <BsMoon className="rounded-full bg-black text-xl font-bold text-white" />
+        ) : (
+          <BsSun className="text-xl dark:bg-white dark:text-black" />
+        )}
+      </button>
+    </>
   );
 }
