d84b37e0bbe0b858544c2eee4dbff431b988682a
Jo <jose_f_dirazar@hotmail.com>
2024-01-03 16:43:10 -0300

    sdfsd

--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -54,11 +54,11 @@ const ProductCard: React.FC<ProductCard> = ({ data }) => {
           <div className="flex justify-center gap-x-6">
             <IconButton
               onClick={onPreview}
-              icon={<Expand size={20} className="text-gray-600" />}
+              icon={<Expand size={30} className="text-gray-600" />}
             />
             <IconButton
               onClick={onAddToCart}
-              icon={<ShoppingCart size={20} className="text-gray-600" />}
+              icon={<ShoppingCart size={30} className="text-gray-600" />}
             />
           </div>
         </div>
--- components/ui/theme-switch.tsx
+++ components/ui/theme-switch.tsx
@@ -27,7 +27,7 @@ export default function ThemeSwitch() {
       <Link
         href="https://wa.me/+5492494280688"
         target="_blank"
-        className="fixed bottom-20 right-[1.2rem] flex h-[3rem] w-[3rem] items-center justify-center rounded-full border-[2px] border-[#35B721] border-opacity-90 bg-green-800"
+        className="fixed bottom-20 right-[1.2rem] flex h-[3.2rem] w-[3.2rem] items-center justify-center rounded-full border-[2px] border-[#35B721] border-opacity-90 bg-green-800"
       >
         <Image
           src="/f3535dc3f95e71506f7c80755610176c.png"
@@ -38,14 +38,14 @@ export default function ThemeSwitch() {
       </Link>
       <Link
         href="/cart"
-        className="fixed bottom-5 right-5 flex h-[3rem] w-[3rem] items-center justify-center rounded-full border-[2px] border-black border-opacity-40 bg-black text-white shadow-2xl transition-all hover:scale-[1.15] active:scale-105 dark:bg-white dark:text-black"
+        className="fixed bottom-5 right-5 flex h-[3.2rem] w-[3.2rem] items-center justify-center rounded-full border-[2px] border-black border-opacity-40 bg-black text-white shadow-2xl transition-all hover:scale-[1.15] dark:bg-white dark:text-black"
       >
         {theme === "light" ? (
-          <ShoppingCart size={18} color="white" className="text-black" />
+          <ShoppingCart size={23} color="white" />
         ) : (
-          <ShoppingCart size={18} color="black" className="text-white" />
+          <ShoppingCart size={23} color="black" />
         )}
-        <span className="ml-1 text-sm font-medium text-white dark:text-black">
+        <span className="ml-1 text-sm font-bold text-white dark:text-black">
           {cart.items.length}
         </span>
       </Link>
